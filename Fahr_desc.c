616a8ce3f41d1d707dac955381e348499c73476a
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-04-06 21:15:39 +0100

    Fahrenheit to Celsius in Reverse Order.

--- Fahr_desc.c
+++ Fahr_desc.c
@@ -12,10 +12,10 @@ int main(){
 
 	printf("%s    %s \n", Fahr, Celsius);
 	
-	for (fahr = 300; fahr >= 0; fahr++){
+	for (fahr = 300; fahr >= 0; fahr -= 20){
 		
-		printf("%4.0d %14.1f \n", fahr , ((5.0/9.0) * (fahr - 32.0)));
-		fahr -= 20;
+		printf("%4d %14.1f \n", fahr , ((5.0/9.0) * (fahr - 32.0)));
+
 	}
 }
 				
