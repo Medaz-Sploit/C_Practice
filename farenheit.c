23d35bd2abb94808b8a35a21e16611746d997cd9
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-04-06 19:43:36 +0100

    New Change

--- farenheit.c
+++ farenheit.c
@@ -13,9 +13,15 @@ int main(){
 	upper = 300;
 
 	fahr = lower;
+
+	char fahr_string[20] = "Fahrenheit";
+	char celsius_string[20] = "Celsius";
+
+	printf("%s   %s \n", fahr_string, celsius_string);
+
 	while (fahr <= upper){
 		celsius = (5.0/9.0) * (fahr + 32);
-		printf("%4.0f %6.1f\n", fahr, celsius);
+		printf("%4.0f %14.1f\n", fahr, celsius);
 		fahr = fahr + step ;
 	}
 }
