d897447e0c64916b8db864a7c51506dfddf6f167
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-04-06 21:29:24 +0100

    Celsius to Fahrenheit in Reverse Order.

--- Celsius_desc.c
+++ Celsius_desc.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0   /* lower limit of the table */
+#define UPPER 300 /* upper limit of the table */
+#define STEP 20   /* step size */
+
 /* Print the table of Celsius to Fahrenheit in Reverse Order 
  * from 300 to 0 degrees. */
 
@@ -11,7 +15,7 @@ int main(){
 
 	printf("%s       %s\n", Celsius, Fahr);
 
-	for(celsius = 300; celsius >= 0; celsius -= 20){
+	for(celsius = UPPER; celsius >= LOWER; celsius -= STEP){
 		
 		printf("%4d %14.1f \n", celsius, ((celsius / (5.0/9.0)) + 32.0));
 	}
