91d8b53a8ab806035237488b1d93a37bfb0b75e2
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2019-04-06 19:14:30 +0100

    New file added

---
+++ farenheit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Print Fahrenheit-Celsius table
+ * for f = 0, 20, ..., 300 */
+
+int main(){
+
+	int lower, upper, step;
+	float celsius;
+
+	step = 20;
+	lower = 0;
+	upper = 300;
+
+	fahr = lower;
+	while (fahr <= upper){
+		celsius = (5.0/9.0) * (fahr -32);
+		fahr = fahr = step ;
+	}
+}
+
